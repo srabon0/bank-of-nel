d45a02f0df96e6c006609ef2360036a5c87c9369
srabon0 <srabonemam4@gmail.com>
2022-02-10 09:26:24 +0600

    withdraw function added

--- js/transaction.js
+++ js/transaction.js
@@ -15,3 +15,51 @@ depositBtn.addEventListener('click',function(){
    balanceTotal.innerText = updatedBal;
     
 })
+
+// widtdraw section
+
+const withdrawBtn = document.getElementById('withdraw-btn');
+withdrawBtn.addEventListener('click',function(){
+   // Check Current Balance
+   const mainBalance = document.getElementById('balance-total');
+   const mainBalanceValue = parseFloat(mainBalance.innerText);
+   // 
+   console.log(mainBalanceValue);
+   const withdrawAmount = document.getElementById('withdraw-amount');
+   const currentWitdraw = parseFloat(withdrawAmount.value);
+   console.log(currentWitdraw);
+
+   if( currentWitdraw>mainBalanceValue ){
+      alert('Not Enough Credit.Transaction failed. ');
+      withdrawAmount.value ='';
+
+   }else{
+      console.log('transaction successfull');
+      const previousBalance = document.getElementById('total-withdraw');
+      const previousBal = parseFloat(previousBalance.innerText);
+   
+      const newBalance = previousBal+currentWitdraw;
+   
+      previousBalance.innerText = newBalance;
+      withdrawAmount.value ='';
+
+
+      const updatedBal = mainBalanceValue-currentWitdraw;
+      mainBalance.innerText = updatedBal;
+   
+   }
+   
+   
+   // const previousBalance = document.getElementById('total-withdraw');
+   // const previousBal = parseFloat(previousBalance.innerText);
+   
+   // const newBalance = previousBal+currentBalance;
+   
+   // previousBalance.innerText = newBalance;
+   // withdrawAmount.value ='';
+   // console.log(newBalance);
+   // // // balance total
+   
+   // balanceTotal.innerText = updatedBal;
+    
+})
